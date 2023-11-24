f6b781daf3aff6711af51ad2718ca7fe1fd00cff
Raw Peke <85494118+merl1337@users.noreply.github.com>
2023-11-24 17:21:22 +0900

    second git
    uploaded

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+import io
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
 from PyQt5.QtCore import Qt
 from random import randint
+from ui_file import Ui_MainWindow
 
 
-class ShowUI(QMainWindow):
+class ShowUI(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
+        self.setupUi(self)
         self.initialize()
 
     def initialize(self):  # Инициализация всех систем и подгрузка UI
-        uic.loadUi("UI.ui", self)  # Загружаем стили
         self.setFixedSize(800, 570)
         self.pushButton.clicked.connect(self.creator)
         canvas = QPixmap(800, 570)
         self.label.setPixmap(canvas)
 
     def creator(self):
-        x, y = [randint(10, 500) for x in range(2)]
-        w, h = [randint(10, 100) for x in range(2)]
-        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
+        x, y = [randint(10, 500) for z in range(2)]
+        w, h = [randint(10, 100) for z in range(2)]
+
         painter = QPainter(self.label.pixmap())
+
         pen = QPen()
         pen.setWidth(1)
-        pen.setColor(Qt.yellow)
+        pen.setColor(QColor(*[randint(0, 255) for z in range(3)]))
         painter.setPen(pen)
         painter.drawEllipse(x, y, w, h)
         painter.end()
